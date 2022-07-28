a87568a3e51788d2f82ae88a2b822781d50d4e37
afrizal0811 <afrizalmaulana0811@gmail.com>
2022-07-28 12:07:51 +0700

    edit layout

--- src/components/LoadingSkeleton/index.js
+++ src/components/LoadingSkeleton/index.js
@@ -4,22 +4,34 @@ import { Card } from "react-bootstrap";
 const LoadingSkeleton = (panjangMobil) => {
   return (
     <div>
-      <div className="d-flex flex-wrap align-items-baseline justify-content-around ">
+      <div className="d-flex flex-wrap align-items-baseline justify-content-around">
         <Card>
-          <Skeleton width={250} height={300} />
-          <Skeleton width={250} height={10} count={5} />
+          <Skeleton width={250} height={275} />
+          <Skeleton width={125} height={15} />
+          <Skeleton width={200} height={15} />
+          <Skeleton width={250} height={10} count={4} />
+          <Skeleton width={250} height={25} />
         </Card>
         <Card>
           <Skeleton width={250} height={300} />
-          <Skeleton width={250} height={10} count={5} />
+          <Skeleton width={125} height={15} />
+          <Skeleton width={200} height={15} />
+          <Skeleton width={250} height={10} count={4} />
+          <Skeleton width={250} height={25} />
         </Card>
         <Card>
           <Skeleton width={250} height={300} />
-          <Skeleton width={250} height={10} count={5} />
+          <Skeleton width={125} height={15} />
+          <Skeleton width={200} height={15} />
+          <Skeleton width={250} height={10} count={4} />
+          <Skeleton width={250} height={25} />
         </Card>
         <Card>
           <Skeleton width={250} height={300} />
-          <Skeleton width={250} height={10} count={5} />
+          <Skeleton width={125} height={15} />
+          <Skeleton width={200} height={15} />
+          <Skeleton width={250} height={10} count={4} />
+          <Skeleton width={250} height={25} />
         </Card>
       </div>
     </div>
